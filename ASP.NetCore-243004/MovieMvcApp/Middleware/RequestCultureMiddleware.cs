c6f97985f010a55ce3b0909ec1e6abb65a0e4315
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-10-08 14:51:27 +0200

    Lab RequestCultureMiddleware

---
+++ ASP.NetCore-243004/MovieMvcApp/Middleware/RequestCultureMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace MovieMvcApp.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RequestCultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestCultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var cultureCode = httpContext.Request.Query["culture"].ToString();
+            if (!string.IsNullOrEmpty(cultureCode))
+            {
+                var culture = new CultureInfo(cultureCode);
+
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+            }
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCultureMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+}
--- ASP.NetCore-243004/MovieMvcApp/Program.cs
+++ ASP.NetCore-243004/MovieMvcApp/Program.cs
@@ -1,5 +1,6 @@
 using BusinessLogic.Contracts;
 using BusinessLogic.Services;
+using MovieMvcApp.Middleware;
 using MovieStore.Contracts;
 using MovieStoreApp.Services;
 
@@ -28,6 +29,8 @@ namespace MovieMvcApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseRequestCultureMiddleware();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
